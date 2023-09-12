52903ae1a5b8efe94ec2f5a34043b72fb4f4c679
Adam Oh <adamohzhihong@gmail.com>
2023-09-12 23:54:24 +0800

    Setup backend

---
+++ peerprep-backend/src/test/java/com/peerprep/peerprepbackend/PeerprepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peerprep.peerprepbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerprepBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
