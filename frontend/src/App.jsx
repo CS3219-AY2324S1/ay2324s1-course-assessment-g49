8cda6c23ec715eb0d436a2ae2242f25940d067b9
kvihashini <kvihashini@gmail.com>
2023-09-13 17:28:15 +0800

    Add category to question component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,66 +1,84 @@
-import { useState, useRef, useEffect } from 'react'
-import QuestionList from './QuestionList'
-import './App.css'
+import { useState, useRef, useEffect } from 'react';
+import QuestionList from './QuestionList';
+import './App.css';
 
 function App() {
-  
-  const storedQuestions = JSON.parse(localStorage.getItem('questions'))
+	const storedQuestions = JSON.parse(localStorage.getItem('questions'));
 
-  const [questions, setQuestions] = useState(storedQuestions !== null ? storedQuestions : [])
+	const [questions, setQuestions] = useState(
+		storedQuestions !== null ? storedQuestions : []
+	);
 
-  /**
-   * { id: 1, title: "question 1", complexity: "easy", description: "aaa" },
-    { id: 2, title: "question 2", complexity: "easy", description: "bbb" },
-   */
+	/**
+	 * { id: 1, title: "question 1", category: "cat1", complexity: "easy", description: "aaa" },
+	 * { id: 2, title: "question 2", category: "cat2", complexity: "easy", description: "bbb" },
+	 */
 
-  const inputRefQuestionId = useRef(null)
-  const inputRefTitle = useRef(null)
-  const inputRefComplexity = useRef(null)
-  const inputRefDescription = useRef(null)
-  
-    const handleSubmit = (evt) => {
-        evt.preventDefault()
-        const question = {id: Number(inputRefQuestionId.current.value), title: inputRefTitle.current.value, complexity: inputRefComplexity.current.value, description: inputRefDescription.current.value}
-        setQuestions([...questions, question])
-        inputRefQuestionId.current.value = ''
-        inputRefTitle.current.value = ''
-        inputRefComplexity.current.value = ''
-        inputRefDescription.current.value = ''
-    }
+	const inputRefQuestionId = useRef(null);
+	const inputRefTitle = useRef(null);
+	const inputRefCategory = useRef(null);
+	const inputRefComplexity = useRef(null);
+	const inputRefDescription = useRef(null);
 
-    useEffect(() => {
-      localStorage.setItem('questions', JSON.stringify(questions))
-    }, [questions])
+	const handleSubmit = (evt) => {
+		evt.preventDefault();
+		const question = {
+			id: Number(inputRefQuestionId.current.value),
+			title: inputRefTitle.current.value,
+			category: inputRefCategory.current.value,
+			complexity: inputRefComplexity.current.value,
+			description: inputRefDescription.current.value,
+		};
+		setQuestions([...questions, question]);
+		inputRefQuestionId.current.value = '';
+		inputRefTitle.current.value = '';
+		inputRefCategory.current.value = '';
+		inputRefComplexity.current.value = '';
+		inputRefDescription.current.value = '';
+	};
 
-  return (
-    <>
-    <h1>Peerprep</h1>
-    <QuestionList questions={questions}/>
-    <form onSubmit={(evt) => {handleSubmit(evt)}}>
-            <input
-            type='text'
-            placeholder='Question Id'
-            ref={inputRefQuestionId}
-            />
-            <input
-            type='text'
-            placeholder='Question Title'
-            ref={inputRefTitle}
-            />
-            <input
-            type='text'
-            placeholder='Question Complexity'
-            ref={inputRefComplexity}
-            />
-            <input
-            type='text'
-            placeholder='Question Description'
-            ref={inputRefDescription}
-            />
-            <button>add item</button>
-        </form>
-    </>
-  )
+	useEffect(() => {
+		localStorage.setItem('questions', JSON.stringify(questions));
+	}, [questions]);
+
+	return (
+		<>
+			<h1>Peerprep</h1>
+			<QuestionList questions={questions} />
+			<form
+				onSubmit={(evt) => {
+					handleSubmit(evt);
+				}}
+			>
+				<input
+					type="text"
+					placeholder="Question Id"
+					ref={inputRefQuestionId}
+				/>
+				<input
+					type="text"
+					placeholder="Question Title"
+					ref={inputRefTitle}
+				/>
+				<input
+					type="text"
+					placeholder="Question Category"
+					ref={inputRefCategory}
+				/>
+				<input
+					type="text"
+					placeholder="Question Complexity"
+					ref={inputRefComplexity}
+				/>
+				<input
+					type="text"
+					placeholder="Question Description"
+					ref={inputRefDescription}
+				/>
+				<button>add item</button>
+			</form>
+		</>
+	);
 }
 
-export default App
+export default App;
--- frontend/src/QuestionList.jsx
+++ frontend/src/QuestionList.jsx
@@ -1,31 +1,31 @@
-import Question from "./Question";
-import { useState } from "react";
+import Question from './Question';
 
 function QuestionList({ questions }) {
-  /**const [questions, setQuestions] = useState([
-    { id: 1, title: "question 1", complexity: "easy", description: "aaa" },
-    { id: 2, title: "question 2", complexity: "easy", description: "bbb" },
+	/**const [questions, setQuestions] = useState([
+    { id: 1, title: "question 1", category: "cat1", complexity: "easy", description: "aaa" },
+    { id: 2, title: "question 2", category: "cat2", complexity: "easy", description: "bbb" },
   ]);*/
 
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Question No.</th>
-            <th>Question Title</th>
-            <th>Question Complexity</th>
-            <th>Question Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {questions.map((question) => {
-            return <Question question={question} />;
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div>
+			<table>
+				<thead>
+					<tr>
+						<th>Question No.</th>
+						<th>Question Title</th>
+						<th>Question Category</th>
+						<th>Question Complexity</th>
+						<th>Question Description</th>
+					</tr>
+				</thead>
+				<tbody>
+					{questions.map((question) => {
+						return <Question question={question} />;
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default QuestionList;
