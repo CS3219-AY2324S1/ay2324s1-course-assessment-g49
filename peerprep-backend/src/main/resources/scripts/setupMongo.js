0e203a8de29bb58dcec980ce15c021321300dad9
Adam Oh <adamohzhihong@gmail.com>
2023-10-04 00:00:21 +0800

    Add mongodb script for preloading sample questions

---
+++ peerprep-backend/src/main/resources/scripts/setupMongo.js
@@ -0,0 +1,126 @@
+use peerprep;
+
+db.createCollection("question");
+
+db.question.insertMany([
+    {
+        "title": "Reverse a String",
+        "description": "placeholder",
+        "categories": ["STRINGS", "ALGORITHMS"],
+        "complexity": "EASY"
+    },
+    {
+        "title": "Linked List Cycle Detection",
+        "description": "placeholder",
+        "categories": ["DATA_STRUCTURES", "ALGORITHMS"],
+        "complexity": "EASY"
+    },
+    {
+        "title": "Roman to Integer",
+        "description": "placeholder",
+        "categories": ["ALGORITHMS"],
+        "complexity": "EASY"
+    },
+    {
+        "title": "Add Binary",
+        "description": "placeholder",
+        "categories": ["BIT_MANIPULATION", "ALGORITHMS"],
+        "complexity": "EASY"
+    },
+    {
+        "title": "Fibonacci Number",
+        "description": "placeholder",
+        "categories": ["RECURSION", "ALGORITHMS"],
+        "complexity": "EASY"
+    },
+    {
+        "title": "Implement Stack using Queues",
+        "description": "placeholder",
+        "categories": ["DATA_STRUCTURES"],
+        "complexity": "EASY"
+    },
+    {
+        "title": "Combine Two Tables",
+        "description": "placeholder",
+        "categories": ["DATABASES"],
+        "complexity": "EASY"
+    },
+    {
+        "title": "Repeated DNA Sequences",
+        "description": "placeholder",
+        "categories": ["ALGORITHMS", "BIT_MANIPULATION"],
+        "complexity": "MEDIUM"
+    },
+    {
+        "title": "Course Schedule",
+        "description": "placeholder",
+        "categories": ["DATA_STRUCTURES", "ALGORITHMS"],
+        "complexity": "MEDIUM"
+    },
+    {
+        "title": "LRU Cache Design",
+        "description": "placeholder",
+        "categories": ["DATA_STRUCTURES"],
+        "complexity": "MEDIUM"
+    },
+    {
+        "title": "Longest Common Subsequence",
+        "description": "placeholder",
+        "categories": ["STRINGS", "ALGORITHMS"],
+        "complexity": "MEDIUM"
+    },
+    {
+        "title": "Rotate Image",
+        "description": "placeholder",
+        "categories": ["ARRAYS", "ALGORITHMS"],
+        "complexity": "MEDIUM"
+    },
+    {
+        "title": "Airplane Seat Assignment Probability",
+        "description": "placeholder",
+        "categories": ["BRAINTEASER"],
+        "complexity": "MEDIUM"
+    },
+    {
+        "title": "Validate Binary Search Tree",
+        "description": "placeholder",
+        "categories": ["DATA_STRUCTURES", "ALGORITHMS"],
+        "complexity": "MEDIUM"
+    },
+    {
+        "title": "Sliding Window Maximum",
+        "description": "placeholder",
+        "categories": ["ARRAYS", "ALGORITHMS"],
+        "complexity": "HARD"
+    },
+    {
+        "title": "N-Queen Problem",
+        "description": "placeholder",
+        "categories": ["ALGORITHMS"],
+        "complexity": "HARD"
+    },
+    {
+        "title": "Serialize and Deserialize a Binary Tree",
+        "description": "placeholder",
+        "categories": ["DATA_STRUCTURES", "ALGORITHMS"],
+        "complexity": "HARD"
+    },
+    {
+        "title": "Wildcard Matching",
+        "description": "placeholder",
+        "categories": ["STRINGS", "ALGORITHMS"],
+        "complexity": "HARD"
+    },
+    {
+        "title": "Chalkboard XOR Game",
+        "description": "placeholder",
+        "categories": ["BRAINTEASER"],
+        "complexity": "HARD"
+    },
+    {
+        "title": "Trips and Users",
+        "description": "placeholder",
+        "categories": ["DATABASES"],
+        "complexity": "HARD"
+    },
+]);
