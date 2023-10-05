0c66cd9196445b7a27e9694bdb9a9f67eeaaad11
Adam Oh <adamohzhihong@gmail.com>
2023-10-05 20:10:57 +0800

    Reimplement login and register endpoints using spring security

--- peerprep-backend/src/main/java/com/peerprep/peerprepbackend/config/SecurityConfig.java
+++ peerprep-backend/src/main/java/com/peerprep/peerprepbackend/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
      * Provides an instance of a BCryptPassword encoder which provides a method to hash a password string
      * using bcrypt hashing function
      *
-     * @return BCryptPassword encoder
+     * @return BCryptPossword encoder
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
