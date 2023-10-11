de8fd00e692284fb5a7ada90ef8b9c90b4937678
LAPTOP-4DN8RPCF\rache <rach3lkeh@gmail.com>
2023-10-11 15:20:09 +0800

    Include update validation

--- peerprep-backend/src/main/java/com/peerprep/peerprepbackend/dto/request/UpdateUserRequest.java
+++ peerprep-backend/src/main/java/com/peerprep/peerprepbackend/dto/request/UpdateUserRequest.java
@@ -1,13 +1,22 @@
 package com.peerprep.peerprepbackend.dto.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Value
 @NoArgsConstructor(force = true)
 public class UpdateUserRequest {
+
+    @Size(min = 1, message = "username cannot be empty")
     String username;
+
+    @Size(min = 1, message = "email cannot be empty")
     String email;
+
+    @Size(min = 1, message = "country cannot be empty")
     String country;
+
+    @Size(min = 1, message = "password cannot be empty")
     String password;
 }
