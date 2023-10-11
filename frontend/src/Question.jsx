0767cefbf9af1da292f5483cd1b3f29774c20b96
Yao Yi <yaoyi2000326@gmail.com>
2023-10-11 16:36:24 +0800

    Add menu for edit and delete question

--- frontend/src/Question.jsx
+++ frontend/src/Question.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Link } from "@mui/material";
+import { IconButton, Link } from "@mui/material";
 import {
   Button,
   Dialog,
@@ -7,11 +7,17 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
+  Menu,
+  MenuItem,
+  ListItemIcon,
 } from "@mui/material";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import { useEffect } from "react";
 import axios from "axios";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function Question({ question, questionId, onDelete }) {
   const categories = question.categories.join(", ");
@@ -21,8 +27,19 @@ function Question({ question, questionId, onDelete }) {
   const [description, setDescription] = React.useState("");
 
   const loadDesciption = async () => {
-    const response = await axios.get("http://localhost:8080/question/" + question.id);
+    const response = await axios.get(
+      "http://localhost:8080/question/" + question.id
+    );
     setDescription(response.data.description);
+  };
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const openActions = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseActions = () => {
+    setAnchorEl(null);
   };
 
   useEffect(() => {
@@ -49,7 +66,42 @@ function Question({ question, questionId, onDelete }) {
       <TableCell>{categories}</TableCell>
       <TableCell align="center">{question.complexity}</TableCell>
       <TableCell>
-        <button onClick={() => onDelete(question.id)}>Delete </button>
+        <IconButton
+          id="long-button"
+          aria-haspopup="true"
+          onClick={handleClick}
+          style={{ outline: "none" }}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="demo-positioned-menu"
+          aria-labelledby="demo-positioned-button"
+          anchorEl={anchorEl}
+          open={openActions}
+          onClose={handleCloseActions}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+        >
+          <MenuItem onClick={handleCloseActions}>
+            <ListItemIcon>
+              <EditIcon fontSize="small" />
+            </ListItemIcon>
+            Edit
+          </MenuItem>
+          <MenuItem onClick={()=>onDelete(question.id)}>
+            <ListItemIcon>
+              <DeleteIcon fontSize="small" />
+            </ListItemIcon>
+            Delete
+          </MenuItem>
+        </Menu>
       </TableCell>
     </TableRow>
   );
