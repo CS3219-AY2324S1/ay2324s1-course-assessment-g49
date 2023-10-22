a733b4a5496c63cef16b56baaa81e5d68a9f7759
Adam Oh <adamohzhihong@gmail.com>
2023-10-22 16:44:56 +0800

    merge master

--- peerprep-backend/src/main/java/com/peerprep/peerprepbackend/config/SecurityConfig.java
+++ peerprep-backend/src/main/java/com/peerprep/peerprepbackend/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
      * Provides an instance of a BCryptPassword encoder which provides a method to hash a password string
      * using bcrypt hashing function
      *
-     * @return BCryptPossword encoder
+     * @return BCryptPassword encoder
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -46,16 +46,24 @@ public class SecurityConfig {
      * Provides an instance of DaoAuthenticationProvider, which is a type of AuthenticationProvider designated to
      * authenticate user information stored in a database
      *
-     * @param userDetailsService
-     * @return
+     * @param userDetailsService a service that loads user data, bean is defined below
+     * @return DaoAuthenticationProvider
      */
     @Bean
     public DaoAuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         return daoAuthenticationProvider;
     }
 
+    /**
+     * Provides an instance of UserDetailsService
+     * It is essentially a functional interface since it only has one abstract method `loadUserByUsername(username)`
+     * Hence, it can be defined as a lambda, taking username as input and retrieving our defined User object
+     *
+     * @return UserDetailService
+     */
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findFirstByUsername(username)
