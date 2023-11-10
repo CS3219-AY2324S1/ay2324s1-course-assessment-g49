3fb72c1b7961c0ac8a3e2f636a11d8e88138fc3e
Adam Oh <adamohzhihong@gmail.com>
2023-11-10 18:55:54 +0800

    Remove integration tests

--- peerprep-backend/src/test/java/com/peerprep/peerprepbackend/PeerprepBackendApplicationTests.java
+++ peerprep-backend/src/test/java/com/peerprep/peerprepbackend/PeerprepBackendApplicationTests.java
@@ -3,7 +3,6 @@ package com.peerprep.peerprepbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PeerprepBackendApplicationTests {
 
     @Test
